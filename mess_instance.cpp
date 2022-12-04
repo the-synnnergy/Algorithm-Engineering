a428924d17346bc1f99e05ed17f0a71a92d0595e
Marcel Dokters <mdokters@uni-osnabrueck.de>
2022-12-04 16:17:35 +0100

    Fixed bfs

--- mess_instance.cpp
+++ mess_instance.cpp
@@ -21,6 +21,7 @@ std::set<Edge> MessInstance::getTerminalPaths()
     for(int i : m_node_configuration){
         if(i)
         {
+            std::cout << "Doing one BFS" << std::endl;
             std::set<Edge> edges = BFSfromTerminal(node_counter, adjacency_list);
             terminal_paths.insert(edges.begin(),edges.end());
         }
@@ -50,32 +51,35 @@ std::set<Edge> MessInstance::getSolutionTerminalPaths()
 
 std::set<Edge> MessInstance::BFSfromTerminal(int node, std::vector<std::vector<int>> adjacency_list_collection)
 {
+    std::vector<bool> visited(m_graph.getNumberOfNodes(), false);
     std::unordered_set<int> visited_nodes;
-    std::vector<int> nodes_to_visit;
+    std::queue<int> nodes_to_visit;
     std::set<Edge> visitable_terminals;
     std::vector<int> start = adjacency_list_collection.at(node);
     for(int n : start)
     {
-            nodes_to_visit.push_back(n);
-            visited_nodes.insert(n);
+            nodes_to_visit.push(n);
+            visited[node] = true;
     }
     while (!nodes_to_visit.empty())
     {
         int current_node = nodes_to_visit.front();
-        nodes_to_visit.erase(nodes_to_visit.begin());
-        visited_nodes.insert(current_node);
+        nodes_to_visit.pop();
+        //visited_nodes.insert(current_node);
         if(m_node_configuration.at(current_node) && current_node != node){
             visitable_terminals.insert(Edge(node,current_node));
         }
         std::vector<int> adjacency_list = adjacency_list_collection.at(current_node);
         for(auto n : adjacency_list){
             //contains not supported in gcc 9.4 i think
-            const bool is_in = (visited_nodes.find(n) != visited_nodes.end());
-            if(is_in)
+            //const bool is_in = (visited_nodes.find(n) != visited_nodes.end());
+            if(visited[n])
             {
                 continue;
             }
-            nodes_to_visit.push_back(n);
+            visited[n] = true;
+            nodes_to_visit.push(n);
+
         }
     }
     return visitable_terminals;
